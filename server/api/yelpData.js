27f7a27dcb75506afec3539594d4ee4ee74c3de9
Maria Quadriq <mquadri23@gmail.com>
2020-11-25 16:38:23 -0600

    modified yelpData.js

--- server/api/yelpData.js
+++ server/api/yelpData.js
@@ -14,40 +14,15 @@ let yelpREST = axios.create({
 
 yelpREST("/businesses/search", {
   params: {
-    location: "kyoto",
-    term: "coffee",
+    location: "new york",
+    term: "vegan",
     limit: 10,
   },
 }).then(({ data }) => {
   let { businesses } = data;
-  console.log('businesses:',businesses)
+  console.log('businesses categories:',businesses)
   businesses.forEach((b) => {
     console.log("Name: ", b.name);
   });
 });
 
-//import axios from "axios";
-
-// const options = {
-//   method: 'GET',
-//   url: 'https://api.yelp.com/v3/businesses/search',
-//   headers: {
-//     Authorization: `Bearer ${API_KEY}`,
-//     "Content-type": "application/json"
-//   },
-//   data: {
-//     accessToken: 'ls257Ls4aesR09k4XvsoTRKD0Gweg-ogvQi4Lafbkdhi-71_vYHHmRZhUOAU0G1PolGfxHsWjbzbVloxtihgiuscn5FBaeGCTzmr2S2fy87SWHcDoAetV4QlK3y9X3Yx',
-//     locale: 'kyoto',
-//     radius: '4000',
-//     term: 'restaurants',
-//     limit: '1',
-//     categories: 'sushi',
-//     sortBy: 'rating'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
