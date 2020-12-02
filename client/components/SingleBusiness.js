db654faef9f0ac16b72627af3f6b6ccc2b214ba7
Maria Quadriq <mquadri23@gmail.com>
2020-12-02 13:43:37 -0600

    SingleBusiness: added function to convert time

--- client/components/SingleBusiness.js
+++ client/components/SingleBusiness.js
@@ -1,32 +1,52 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchSingleBusiness} from '../store/singleBusiness'
+import {fetchSingleBusiness,fetchSingleBusinessDetails} from '../store/singleBusiness'
 
 
 class SingleBusiness extends Component {
-// constructor(props){
-//     super()
-// }
+constructor(props){
+    super()
+}
 componentDidMount() {
     this.props.getSingleBusiness(this.props.match.params.businessId)
+  }
+
+  convertTime(time){
+    let numValue = Number(time)/100
+    let AmOrPm = numValue >= 12 ? 'pm' : 'am';
+    let finalTime = AmOrPm === 'pm'? String(numValue-12)+AmOrPm : String(numValue)+AmOrPm
+    return finalTime
   }
 
   render(){
       const {singleBusiness} = this.props
       console.log('singleBusiness:', singleBusiness)
-    
+     
       return(
           <div>
       <h1>{singleBusiness.name}</h1>
       {singleBusiness.categories?(
-      <h4>Category:{singleBusiness.categories.join('|')}</h4>
+        <div>
+      <h4>Category:{singleBusiness.categories.map(obj => obj.title).join('|')}</h4>
+
+      {/* <img src={singleBusiness.photos.map(imageUrl => imageUrl)}/> */}
+
+      <h4>Hours: {singleBusiness.hours[0].open.map(hrsObj => this.convertTime(hrsObj.start))}</h4>
+
+        <h1>{this.convertTime('0700')}</h1>
+
+      <h4>Street Address: {singleBusiness.location.address1}</h4>
+      <h4>Phone: {singleBusiness.display_phone}</h4>
+      <h4>Rating: {singleBusiness.rating}</h4>
+      <h4>Price: {singleBusiness.price}</h4>
+
+      <img className="resize" src={singleBusiness.image_url}/>
+
+      </div>
       ):(
         <h1>Error</h1>
       )}
-      <h4>Street Address: {singleBusiness.streetAddress}</h4>
-      <h4>Rating: {singleBusiness.rating}</h4>
-      <h4>Price: {singleBusiness.price}</h4>
-      <img className="resize" src={singleBusiness.imageUrl}/>
+      
       </div>
       )
   }
@@ -39,7 +59,8 @@ const mapState = (state) => {
   }
   const mapDispatch = dispatch => {
     return {
-      getSingleBusiness: (id) => dispatch(fetchSingleBusiness(id))
+      // getSingleBusiness: (id) => dispatch(fetchSingleBusiness(id))
+      getSingleBusiness: (id) => dispatch(fetchSingleBusinessDetails(id))
     }
   }
   export default connect(mapState, mapDispatch)(SingleBusiness)
--- client/store/singleBusiness.js
+++ client/store/singleBusiness.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
 //Action type
-const GET_BUSINESS = 'GET_BUSINESS'
+const GET_BUSINESS = "GET_BUSINESS";
+const GET_BUSINESS_DETAILS = "GET_BUSINESS_DETAILS";
 
 //Action Creator
 export const getBusiness = (business) => {
   return {
     type: GET_BUSINESS,
+    business,
+  };
+};
+
+export const getBusinessDetails = (business) => {
+  return {
+    type: GET_BUSINESS_DETAILS,
     business,
   };
 };
@@ -23,10 +31,23 @@ export const fetchSingleBusiness = (id) => {
   };
 };
 
+export const fetchSingleBusinessDetails = (id) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/yelp/search/businesses/${id}`);
+      dispatch(getBusinessDetails(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
 const initialState = {}; //object for single business
 export default function businessReducer(state = initialState, action) {
   switch (action.type) {
     case GET_BUSINESS:
+      return action.business;
+    case GET_BUSINESS_DETAILS:
       return action.business;
     default:
       return state;
