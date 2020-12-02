f6587c612755e3b213188ceefe3ea2f813168298
Aarluk <provencenewyork@gmail.com>
2020-12-02 12:57:59 -0500

    BUG: fixed user.js bug after last pull

--- client/store/user.js
+++ client/store/user.js
@@ -1,31 +1,65 @@
 import axios from "axios";
 
-//ACTION TYPES
+//ACTION TYPE
+const REMOVE_USER = 'REMOVE_USER';
 const GOT_USER = "GOT_USER";
 
+//INITIAL STATE
+const defaultUser = {};
+
 //ACTION CREATORS
-const gotUser = (user) => ({ type: GOT_USER, user });
+const getUser = user => ({ type: GOT_USER, user });
+const removeUser = () => ({type: REMOVE_USER})
 
 //THUNK CREATORS
-export const auth = (body) => async (dispatch) => {
+export const me = () => async dispatch => {
 	try {
-		const { data } = await axios.post("/auth/login", body);
-		dispatch(gotUser(data));
-	} catch (error) {
-		console.error(error);
+	  const res = await axios.get('/auth/me')
+	  dispatch(getUser(res.data || defaultUser))
+	} catch (err) {
+	  console.error(err)
 	}
-};
+  }
+
+export const auth = (body) => async dispatch => {
+	try {
+		const { data } = await axios.post(`/auth/login`, body)
+    dispatch(gotUser(data))
+  } catch (error) {
+   console.error(error)
+  }
+}
+//   try {
+//     dispatch(getUser(res.data))
+//     history.push('/')
+//   } catch (dispatchOrHistoryErr) {
+//     console.error(dispatchOrHistoryErr)
+//   }
+// }
+
+// export const logout = () => async dispatch => {
+// 	try {
+// 	  await axios.post('/auth/logout')
+// 	  dispatch(removeUser())
+// 	  history.push('/login')
+// 	} catch (err) {
+// 	  console.error(err)
+// 	}
+//   }
 
 export const register = (body) => async (dispatch) => {};
 
-//REDUCER
-const initialState = {};
+// const initialState = {};
 
-export default function userReducer(state = initialState, action) {
+//REDUCER
+export default function userReducer(state = defaultUser, action) {
 	switch (action.type) {
 		case GOT_USER:
 			return action.user;
+		case REMOVE_USER:
+      		return defaultUser
 		default:
 			return state;
 	}
 }
+
