a083ceda83b2842d11f89c0605e68886d46716c3
Aarluk <provencenewyork@gmail.com>
2020-12-04 18:42:16 -0500

    git commit -m"STYLE" saving friday night changes for styling in nav bar and profile page"

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -5,41 +5,49 @@ import Button from "react-bootstrap/Button";
 import { logout } from "../store";
 
 class Profile extends React.Component {
-	render() {
-		const user = this.props.user;
-		return (
-			<div id="container-profile">
-				<img className="img-profile" src={user.imageUrl} width="100px" />
-				<p className="welcome"> {user.firstName} {user.lastName} </p>
-				<p className="member">
-					Been fooding since:{" "}
-					{new Date(user.registrationDate).toLocaleDateString()}
-				</p>
-				<p>
-					<Link to="/myMaps">Go To My Saved Maps</Link>
-				</p>
-				<p>
-					<Link to="/editProfile">Edit My Profile</Link>
-				</p>
-				<Button
-					onClick={() => {
-						this.props.logout();
-						this.props.history.push("/");
-					}}
-				>
-					Logout
-				</Button>
-			</div>
-		);
-	}
+  render() {
+    const user = this.props.user;
+    return (
+      <div id="container-profile">
+        <img className="img-profile" src={user.imageUrl} width="100px" />
+        <p className="welcome">
+          {" "}
+          {user.firstName} {user.lastName}{" "}
+        </p>
+        <p className="member">
+          Been fooding since:{" "}
+          {new Date(user.registrationDate).toLocaleDateString()}
+        </p>
+
+        <div className="pink-separation">
+          <p  className="text-pink-separation">My Foodie Maps</p>
+        </div>
+
+        <p className="saved-maps">
+          <Link to="/myMaps">Go To My Saved Maps</Link>
+        </p>
+        <p>
+          <Link to="/editProfile">Edit My Profile</Link>
+        </p>
+        <Button
+          onClick={() => {
+            this.props.logout();
+            this.props.history.push("/");
+          }}
+        >
+          Logout
+        </Button>
+      </div>
+    );
+  }
 }
 
 const mapState = (state) => ({
-	user: state.user,
+  user: state.user,
 });
 
 const mapDispatch = (dispatch) => ({
-	logout: () => dispatch(logout()),
+  logout: () => dispatch(logout()),
 });
 
 export default withRouter(connect(mapState, mapDispatch)(Profile));
