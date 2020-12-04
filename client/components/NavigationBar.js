9aca491eff044d33b4e5309ca8ba4ed4eaba834b
Aarluk <provencenewyork@gmail.com>
2020-12-04 14:33:34 -0500

    STYLE: added Bootstrap icon and navbar at bottom for mobile only

--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -6,55 +6,55 @@ import { logout } from "../store";
 
 import React from "react";
 
-
 class NavigationBar extends React.Component {
-	render() {
-		return (
-			<Navbar bg="dark" variant="dark" expand="lg" id="navbar-desktop">
-				<Navbar.Brand as={NavLink} to="/">
-					Food Path
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="mr-auto">
-						<Nav.Link as={NavLink} to="/">
-							Home
-						</Nav.Link>
+  render() {
+    return (
+      <Navbar id="navbar" bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand as={NavLink} to="/" id="logo-nav">
+          Food Path
+        </Navbar.Brand>
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        <Navbar id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={NavLink} to="/">
+              <img src="./icons/house.svg" width="30" className="home-icon" />
+              <p className="home-text">Home</p>
+            </Nav.Link>
 
-						{this.props.user.id && (
-							<>
-								<Nav.Link as={NavLink} to="/search">
-									Search
-								</Nav.Link>
-								<Nav.Link as={NavLink} to="/map">
-									Map
-								</Nav.Link>
-								<Nav.Link as={NavLink} to="/profile">
-									Profile
-								</Nav.Link>
-								<Nav.Link
-									onClick={() => {
-										this.props.logout();
-										this.props.history.push("/");
-									}}
-								>
-									Logout
-								</Nav.Link>
-							</>
-						)}
-					</Nav>
-				</Navbar.Collapse>
-			</Navbar>
-		);
-	}
+            {this.props.user.id && (
+              <>
+                <Nav.Link as={NavLink} to="/search">
+                  Search
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/map">
+                  Map
+                </Nav.Link>
+                <Nav.Link as={NavLink} to="/profile">
+                  Profile
+                </Nav.Link>
+                <Nav.Link
+                  onClick={() => {
+                    this.props.logout();
+                    this.props.history.push("/");
+                  }}
+                >
+                  Logout
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar>
+      </Navbar>
+    );
+  }
 }
 
 const mapState = (state) => ({
-	user: state.user,
+  user: state.user,
 });
 
 const mapDispatch = (dispatch) => ({
-	logout: () => dispatch(logout()),
+  logout: () => dispatch(logout()),
 });
 
 export default withRouter(connect(mapState, mapDispatch)(NavigationBar));
