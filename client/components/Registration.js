139b46ae1949f6a08a817dfbe0684754687ceb11
Aarluk <provencenewyork@gmail.com>
2020-12-07 18:28:47 -0500

    DESIGN: designing signup page

--- client/components/Registration.js
+++ client/components/Registration.js
@@ -2,82 +2,94 @@ import React from "react";
 import { connect } from "react-redux";
 import { register } from "../store";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 class Registration extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			firstName: "",
-			lastName: "",
-			email: "",
-			password: "",
-		};
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
+  constructor() {
+    super();
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-	onChange(event) {
-		this.setState({
-			[event.target.name]: event.target.value,
-		});
-	}
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
 
-	onSubmit(event) {
-		event.preventDefault();
-		this.props.auth(this.state);
-		this.setState({
-			firstName: "",
-			lastName: "",
-			email: "",
-			password: "",
-		});
-	}
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.auth(this.state);
+    this.setState({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+    });
+  }
 
-	render() {
-		return (
-			<>
-				<form onSubmit={this.onSubmit}>
-					<input
-						name="firstName"
-						type="text"
-						placeholder="First name"
-						value={this.state.firstName}
-						onChange={this.onChange}
-					/>
-					<input
-						name="lastName"
-						type="text"
-						placeholder="Last name"
-						value={this.state.lastName}
-						onChange={this.onChange}
-					/>
-					<input
-						name="email"
-						type="email"
-						placeholder="Email"
-						value={this.state.email}
-						onChange={this.onChange}
-					/>
-					<input
-						name="password"
-						type="password"
-						placeholder="Password"
-						value={this.state.password}
-						onChange={this.onChange}
-					/>
-					<button type="submit">Sign Up</button>
-				</form>
+  render() {
+    return (
+      <>
+        <div id="purple-bg">
+          <Form.Group onSubmit={this.onSubmit} className="content-signup">
+            <Form.Control
+              className="firstname"
+              name="firstName"
+              type="text"
+              placeholder="First name"
+              value={this.state.firstName}
+              onChange={this.onChange}
+            />
+            <Form.Control
+              className="lastname"
+              name="lastName"
+              type="text"
+              placeholder="Last name"
+              value={this.state.lastName}
+              onChange={this.onChange}
+            />
+            <Form.Control
+              className="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.onChange}
+            />
+            <Form.Control
+              className="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.onChange}
+            />
+            <Button id="btn-signup" variant="primary" type="submit">
+              Sign Up
+            </Button>
+          </Form.Group>
 
-				<form method="get" action="/auth/google">
-					<Button type="submit">Login with Google</Button>
-				</form>
-			</>
-		);
-	}
+          <Form method="get" action="/auth/google">
+            <Button id="btn-google" type="submit">
+              Signup with Google
+            </Button>
+
+          </Form>
+        </div>
+      </>
+    );
+  }
 }
 
 const mapDispatchToProps = (dispatch) => ({
-	auth: (body) => dispatch(register(body)),
+  auth: (body) => dispatch(register(body)),
 });
 
 export default connect(null, mapDispatchToProps)(Registration);
