43afc60d274561ad8a67bdeacb5b0aea6a48babd
Annie Ilizarov <annie.ilizarov@gmail.com>
2020-12-07 14:19:20 -0500

    PROFILEVIEW_LIST_OF_MAPS: in profile view, can now see list of all the saved maps

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -3,42 +3,45 @@ import { connect } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { logout } from "../store";
+import { MyMaps } from ".";
 
 class Profile extends React.Component {
-  render() {
-    const user = this.props.user;
-    return (
-      <div id="container-profile">
-        <img className="img-profile" src={user.imageUrl} width="100px" />
-        <h2 className="name">
-          {" "}
-          {user.firstName} {user.lastName}{" "}
-        </h2>
-        <div>
-          <p className="member">
-            Been fooding since:{" "}
-            {new Date(user.registrationDate).toLocaleDateString()}
-          </p>
-        </div>
-        <div className="div-text">
-          <p className="text-separation">My Foodie Maps 
-          {/* <img src="./icons/geo-alt.svg" width="20" className="foodie-icon" /> */}
-          </p>
-        </div>
-        <p className="saved-maps">
-          <Link to="/myMaps">Go To My Saved Maps</Link>
-        </p>
-        <div className="div-text">
-          <p className="text-separation">My Account 
-          {/* <img src="./icons/person-bounding-box.svg" width="20" className="account-icon" /> */}
-          </p>
-        </div>
+	render() {
+		const user = this.props.user;
+		return (
+			<div id="container-profile">
+				<img className="img-profile" src={user.imageUrl} width="100px" />
+				<h2 className="name">
+					{" "}
+					{user.firstName} {user.lastName}{" "}
+				</h2>
+				<div>
+					<p className="member">
+						Been fooding since:{" "}
+						{new Date(user.registrationDate).toLocaleDateString()}
+					</p>
+				</div>
+				<div className="div-text">
+					<p className="text-separation">
+						My Foodie Maps
+						{/* <img src="./icons/geo-alt.svg" width="20" className="foodie-icon" /> */}
+					</p>
+				</div>
+				<span className="saved-maps">
+					<MyMaps />
+				</span>
+				<div className="div-text">
+					<p className="text-separation">
+						My Account
+						{/* <img src="./icons/person-bounding-box.svg" width="20" className="account-icon" /> */}
+					</p>
+				</div>
 
-        <p>
-          <Link to="/editProfile">Edit My Profile</Link>
-        </p>
+				<p>
+					<Link to="/editProfile">Edit My Profile</Link>
+				</p>
 
-        {/* <Button
+				{/* <Button
           variant="outline-secondary btn-sm"
           className="btn-logout"
           onClick={() => {
@@ -48,17 +51,17 @@ class Profile extends React.Component {
         >
           Logout
         </Button> */}
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 const mapState = (state) => ({
-  user: state.user,
+	user: state.user,
 });
 
 const mapDispatch = (dispatch) => ({
-  logout: () => dispatch(logout()),
+	logout: () => dispatch(logout()),
 });
 
 export default withRouter(connect(mapState, mapDispatch)(Profile));
