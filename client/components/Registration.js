69468c8c943880d694e422a28c18cfc6d75bfdfa
Annie Ilizarov <annie.ilizarov@gmail.com>
2020-12-11 16:52:56 -0500

    ADDED_ERROR_MSG: when you sign up as a new user but you already exist, an alert will display

--- client/components/Registration.js
+++ client/components/Registration.js
@@ -6,109 +6,111 @@ import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 
 class Registration extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    };
-    this.onChange = this.onChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+	constructor() {
+		super();
+		this.state = {
+			firstName: "",
+			lastName: "",
+			email: "",
+			password: "",
+		};
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
 
-  onChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
+	onChange(event) {
+		this.setState({
+			[event.target.name]: event.target.value,
+		});
+	}
 
-  onSubmit(event) {
-    event.preventDefault();
-    this.props.auth(this.state);
-    this.setState({
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-    });
-    this.props.history.push("/profile");
-  }
+	async onSubmit(event) {
+		event.preventDefault();
+		try {
+			await this.props.auth(this.state);
+			this.props.history.push("/profile");
+		} catch (error) {
+			alert("Error! Please Try Again!");
+		}
+	}
 
-  render() {
-    return (
-      <>
-        <div id="picture-bg">
-          <div id="purple-bg">
-            <img src="/img/logo2.png" width="80" className="logo-signup" />
+	render() {
+		return (
+			<>
+				<div id="picture-bg">
+					<div id="purple-bg">
+						<img src="/img/logo2.png" width="80" className="logo-signup" />
 
-            <p className="welcome-msg">
-              Welcome to Food Path! <br></br>Tell us a little bit about
-              yourself.
-            </p>
+						<p className="welcome-msg">
+							Welcome to Food Path! <br></br>Tell us a little bit about
+							yourself.
+						</p>
 
-            <Form.Group onSubmit={this.onSubmit} className="content-signup">
-              <Form.Control
-                className="firstname"
-                name="firstName"
-                type="text"
-                placeholder="First name"
-                value={this.state.firstName}
-                onChange={this.onChange}
-              />
-              <Form.Control
-                className="lastname"
-                name="lastName"
-                type="text"
-                placeholder="Last name"
-                value={this.state.lastName}
-                onChange={this.onChange}
-              />
-              <Form.Control
-                className="email"
-                name="email"
-                type="email"
-                placeholder="Email"
-                value={this.state.email}
-                onChange={this.onChange}
-              />
-              <Form.Control
-                className="password"
-                name="password"
-                type="password"
-                placeholder="Password"
-                value={this.state.password}
-                onChange={this.onChange}
-              />
-              <Button id="btn-signup" type="submit" onClick={this.onSubmit}>
-                Sign Up
-              </Button>
-            </Form.Group>
+						<Form onSubmit={this.onSubmit} className="content-signup">
+							<Form.Control
+								className="firstname"
+								name="firstName"
+								type="text"
+								placeholder="First name"
+								value={this.state.firstName}
+								onChange={this.onChange}
+							/>
+							<Form.Control
+								className="lastname"
+								name="lastName"
+								type="text"
+								placeholder="Last name"
+								value={this.state.lastName}
+								onChange={this.onChange}
+							/>
+							<Form.Control
+								className="email"
+								name="email"
+								type="email"
+								placeholder="Email"
+								value={this.state.email}
+								onChange={this.onChange}
+							/>
+							<Form.Control
+								className="password"
+								name="password"
+								type="password"
+								placeholder="Password"
+								value={this.state.password}
+								onChange={this.onChange}
+							/>
+							<Button id="btn-signup" type="submit">
+								Sign Up
+							</Button>
+						</Form>
 
-            <Form method="get" action="/auth/google" id="form-google-btn">
-              <Button id="btn-google" type="submit">
-                <img
-                  src="./img/google.png"
-                  width="15"
-                  className="google-icon"
-                />
-                Sign up with Google
-              </Button>
+						<Form method="get" action="/auth/google" id="form-google-btn">
+							<Button id="btn-google" type="submit">
+								<img
+									src="./img/google.png"
+									width="15"
+									className="google-icon"
+								/>
+								Sign up with Google
+							</Button>
 
-              <Button id="btn-already-account" type="submit">
-                <Link to="/login">Already a member? Sign in here</Link>
-              </Button>
-            </Form>
-          </div>
-        </div>
-      </>
-    );
-  }
+							<Button id="btn-already-account" type="submit">
+								<Link to="/login">Already a member? Sign in here</Link>
+							</Button>
+						</Form>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
-const mapDispatchToProps = (dispatch) => ({
-  auth: (body) => dispatch(register(body)),
+const mapStateToProps = (state) => ({
+	user: state.user,
 });
 
-export default connect(null, mapDispatchToProps)(Registration);
+const mapDispatchToProps = (dispatch) => ({
+	auth: (body) => dispatch(register(body)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Registration);
