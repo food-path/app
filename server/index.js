c948726ef55a5659a2e968cc47c178e88520d6fe
Annie Ilizarov <annie.ilizarov@gmail.com>
2020-12-11 15:47:25 -0500

    edited http to https redirect

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const dbStore = new SequelizeStore({ db: db });
 const passport = require("passport");
 const enforce = require("express-sslify");
+const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS;
 
 //TODO: trying to figure out how to redirect http to https
 // app.use((req, res, next) => {
@@ -47,7 +48,10 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV !== "development") app.use(enforce.HTTPS());
+if (process.env.NODE_ENV !== "development") {
+	app.use(redirectToHTTPS([], [], 301));
+	app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 
 app.use(
 	session({
