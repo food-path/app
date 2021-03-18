68834fba02380ff8b8eeb9f2234f302dce39bd25
Carolyn Berard <carolynmberard@gmail.com>
2021-03-18 15:54:52 -0500

    Update: updated our database for SSL PostgreSQL update

--- server/db/database.js
+++ server/db/database.js
@@ -1,10 +1,26 @@
 const Sequelize = require("sequelize");
 
+let config
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false, //put true if you like the logs
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+} else {
+  config = {
+    logging: false
+  }
+}
+
 const db = new Sequelize(
 	process.env.DATABASE_URL || "postgres://localhost:5432/foodpath",
-	{
-		logging: false, //put true if you like the logs
-	}
+	config
 );
 
 module.exports = db;
